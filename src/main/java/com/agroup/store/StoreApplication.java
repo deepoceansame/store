ce9e718d761696c84241f8cd040c529f43d42857
deepoceansame <11911817@mail.sustech.edu.cn>
2021-10-17 17:58:18 +0800

    update StoreApplication

--- src/main/java/com/agroup/store/StoreApplication.java
+++ src/main/java/com/agroup/store/StoreApplication.java
@@ -1,13 +1,20 @@
 package com.agroup.store;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class StoreApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(StoreApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+        SpringApplication app = new SpringApplication(StoreApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功!!");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
