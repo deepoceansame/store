772fa13c198c1a87188e4b0c909d80ad1ed657e1
deepoceansame <11911817@mail.sustech.edu.cn>
2021-12-04 17:29:35 +0800

    开始inqury的列表

--- web/src/views/BuyInqury.vue
+++ web/src/views/BuyInqury.vue
@@ -1,21 +1,64 @@
 <template>
-  <category-nav></category-nav>
-  <br/>
-  <search></search>
   <buy-nav></buy-nav>
   <div>buy inqury list here</div>
+  {{DesiredGoodsList}}
 </template>
 
 <script>
 import BuyNav from "@/components/BuyNav";
 import CategoryNav from "@/components/CategoryNav";
 import Search from "@/components/Search";
+import {computed, onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+import store from "@/store";
+import axios from "axios";
+import {message} from "ant-design-vue";
 export default {
   name: "BuyInqury",
   components:{
     BuyNav,
-    CategoryNav,
-    Search,
+  },
+  setup(){
+
+    const DesiredGoodsList = ref([])
+    const route = useRoute()
+    const account = computed(() => {
+      return store.state.account
+    })
+
+    const handleQuery = (params) => {
+      DesiredGoodsList.value = [];
+      axios.get("/desiredGoods/showDesiredGoodsListByAccountId",{
+        params:{
+          accountId: params.accountId
+        }
+      }).then(
+          (response) =>  {
+            const data = response.data
+            if (data.success){
+              DesiredGoodsList.value = data.content.list;
+            } else{
+              message.error(data.message)
+            }
+          }
+      )
+    }
+
+    onMounted(
+        () => {
+          console.log('mounted')
+          if(typeof(route.params.keyword) == 'undefined' || route.params.keyword.trim().length === 0){
+            handleQuery({accountId: account.value.id});
+          }
+          else{
+            handleQuery({accountId: account.value.id})
+          }
+        }
+    )
+
+    return {
+      DesiredGoodsList
+    }
   }
 }
 </script>
