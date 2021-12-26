7b1c1f5f35e4a2bdb8bf8a4e98de45106a6d2f79
deepoceansame <11911817@mail.sustech.edu.cn>
2021-12-26 17:02:55 +0800

    转账时发邮件

--- src/main/java/com/agroup/store/service/AccountService.java
+++ src/main/java/com/agroup/store/service/AccountService.java
@@ -290,8 +290,35 @@ public class AccountService {
             resp.setMessage("钱转过去了 余额" + afterSenderMoney);
             resp.setSuccess(true);
             LOG.info("转了");
+            Account sender = accountMapper.selectByPrimaryKey(senderid);
+            Account receiver = accountMapper.selectByPrimaryKey(receiverid);
+            sendMailWhenTransferMoney(sender, receiver, famount);
         }
         return resp;
+    }
+
+    public void sendMailWhenTransferMoney(Account sender, Account receiver, Float amount) {
+        // 构建一个邮件对象
+        SimpleMailMessage message = new SimpleMailMessage();
+        // 设置邮件主题
+        message.setSubject("通知：有人转账给你");
+        // 设置邮件发送者，这个跟application.yml中设置的要一致
+        message.setFrom(nickname + '<' + senderUsername + '>');
+        // 设置邮件接收者，可以有多个接收者，中间用逗号隔开，以下类似
+        // message.setTo("10*****16@qq.com","12****32*qq.com");
+        message.setTo(receiver.getMail());
+        // 设置邮件抄送人，可以有多个抄送人
+//        message.setCc("12****32*qq.com");
+//        // 设置隐秘抄送人，可以有多个
+//        message.setBcc("7******9@qq.com");
+//        // 设置邮件发送日期
+        message.setSentDate(new Date());
+        // 设置邮件的正文
+
+        String content = "用户（用户名：" + sender.getName() + "）向你转账"+amount;
+        message.setText(content);
+        // 发送邮件
+        javaMailSender.send(message);
     }
 
     public void sendMailToBuyerForNewSupplier(Account buyer, Account seller, DesiredGoods desiredGoods) {
